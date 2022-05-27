efb0796ad0c4edee3197cbcc4e457f122791a52d
Elisha Adeboye <y.adeboye@gmail.com>
2022-05-27 06:42:25 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, tracks, score):
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
+
+    def change_name(self, new_name):
+        self.name = new_name
+
+    def change_age(self, new_age):
+        self.age = int(new_age)
+
+    def add_track(self, new_track):
+        return self.tracks.append(new_track)
+
+    def get_score(self):
+        return self.score
+
+
+Bob = Student(name="Bob", age=26, tracks=["Fastapp","Bread"],score=20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
+
+
